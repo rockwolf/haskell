6738461edc49859b3da9b8b0c1c5174903aec46f
rockwolf <thereisanewway@gmail.com>
2013-02-28 13:49:25 +0100

    Added an example function with guards.

--- invade/invade.hs
+++ invade/invade.hs
@@ -10,6 +10,14 @@ boilerPlate = ["Case #" ++ show n ++ ": " | n <- [1..]]
 standardOutput :: [String] -> [String]
 standardOutput = zipWith (++) boilerPlate
 
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+
 main = do
     (p:_) <- getArgs
     pool <- readFile f
@@ -18,3 +26,5 @@ main = do
         solutions = standardOutput $ map reverseWords cases
     
     putStrLn $ unlines $ solutions
+
+    putStrLn $ (bmiTell 75.68 170)
