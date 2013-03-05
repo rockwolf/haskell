09c0ac696ce744d2394e5ec89d80c2c9523df307
Andy Nagels <thereisanewway@gmail.com>
2013-03-05 20:36:55 +0100

    Continued the tutorial, can display an image now.

--- sokoban/SokobanGui.hs
+++ sokoban/SokobanGui.hs
@@ -25,20 +25,33 @@ import Control.Monad (guard)
     else gameLoop world'
 -}
 
-data State = State {sWorld :: World}
+data State = State {sWorld :: World
+                    ,sInput :: Maybe Input}
 
-emptyState = State {sWorld = loadLevel level}
+emptyState = State {sWorld = loadLevel level
+                    ,sInput = Nothing}
 
-handleKeyBoard = do
+handleKeyBoard state = do
     --[left, right, up, down] <- mapM (liftIO . keyvalFromName)
     --                                ["Left", "Right", "Up", "Down"]
-    tryEvent $ do
-        val <- keyvalName
-        "Left" <- liftIO $ keyvalName val
-        liftIO $ do
-            print "Left"
-     
+    checkInput "Left"   Left
+    checkInput "Right"  Right
+    checkInput "Up"     Up
+    checkInput "Down"   Down
+    where
+        checkInput key i = tryEvent $ do
+            "Left" <- (liftIO . keyvalName) =<< eventKeyVal
+            liftIO $ do
+                MV.modifyMVar_ state (\s -> return s{sInput = return i})
 
+drawWindow window state = liftIO $ do
+    cr <- widgetGetDrawWindow window
+    renderWithDrawable cr $ do
+        image <- liftIO $ C.imageSurfaceCreateFromPNG "images/Grass Block.png"
+        C.setSourceSurface image 0 0
+        C.paint
+        return True
+     
 main :: IO ()
 main = do
     initGUI
@@ -46,7 +59,9 @@ main = do
     state <- MV.newMVar emptyState
     
     window <- windowNew
-    window `on` keyPressEvent $ handleKeyBoard
+    window `on` sizeRequest     $ return (Requisition 1400 900)
+    window `on` keyPressEvent   $ handleKeyBoard state
+    window  `on` exposeEvent    $ drawWindow window state
 
     onDestroy window mainQuit
     widgetShowAll window
