529d33a62696020ee6229bc01cdded88e69a58d5
Andy Nagels <thereisanewway@gmail.com>
2013-03-05 22:24:48 +0100

    We can make the image move... hehe :)

--- sokoban/SokobanGui.hs
+++ sokoban/SokobanGui.hs
@@ -5,8 +5,47 @@ import Prelude hiding (Either(..))
 import Graphics.Rendering.Cairo as C
 import Graphics.UI.Gtk
 import Control.Concurrent.MVar as MV
-import Control.Monad (guard)
+import Control.Monad (when, liftM)
 
+data State = State {sWorld :: World
+                    ,sInput :: Maybe Input
+                    ,sOffset :: Double}
+
+emptyState = State {sWorld = loadLevel level
+                    ,sInput = Nothing
+                    ,sOffset = 0}
+
+handleKeyBoard window state = do
+    --[left, right, up, down] <- mapM (liftIO . keyvalFromName)
+    --                                ["Left", "Right", "Up", "Down"]
+    tryEvent $ do
+        "Left" <- (liftIO . keyvalName) =<< eventKeyVal
+        checkInput Left
+    tryEvent $ do
+        "Right" <- (liftIO . keyvalName) =<< eventKeyVal
+        checkInput Right
+    tryEvent $ do
+        "Up" <- (liftIO . keyvalName) =<< eventKeyVal
+        checkInput Up
+    tryEvent $ do
+        "Down" <- (liftIO . keyvalName) =<< eventKeyVal
+        checkInput Down
+    where
+        checkInput i = liftIO $ do
+            MV.modifyMVar_ state (\s -> return s{sInput = return i})
+            MV.modifyMVar_ state (\s -> return s{sOffset = sOffset s + 10})
+            updateWorld state
+            widgetQueueDraw window
+
+updateWorld state = do
+    s <- readMVar state
+    let input = sInput s
+        world = sWorld s
+        world' = case modifyWorld world =<< input of
+                    Nothing -> world
+                    Just x  -> x
+    MV.modifyMVar_ state (\s -> return s{sWorld = world'})
+    when (isFinished world') $ print "Finished."
 {-
  gameLoop world = do
     print world
@@ -25,32 +64,15 @@ import Control.Monad (guard)
     else gameLoop world'
 -}
 
-data State = State {sWorld :: World
-                    ,sInput :: Maybe Input}
-
-emptyState = State {sWorld = loadLevel level
-                    ,sInput = Nothing}
-
-handleKeyBoard state = do
-    --[left, right, up, down] <- mapM (liftIO . keyvalFromName)
-    --                                ["Left", "Right", "Up", "Down"]
-    checkInput "Left"   Left
-    checkInput "Right"  Right
-    checkInput "Up"     Up
-    checkInput "Down"   Down
-    where
-        checkInput key i = tryEvent $ do
-            "Left" <- (liftIO . keyvalName) =<< eventKeyVal
-            liftIO $ do
-                MV.modifyMVar_ state (\s -> return s{sInput = return i})
-
 drawWindow window state = liftIO $ do
     cr <- widgetGetDrawWindow window
+    offset <- liftM sOffset $ readMVar state
     renderWithDrawable cr $ do
         image <- liftIO $ C.imageSurfaceCreateFromPNG "images/Grass Block.png"
-        C.setSourceSurface image 0 0
+        C.setSourceSurface image offset offset
         C.paint
-        return True
+    return True
+--continue at 0:41:21
      
 main :: IO ()
 main = do
@@ -60,8 +82,8 @@ main = do
     
     window <- windowNew
     window `on` sizeRequest     $ return (Requisition 1400 900)
-    window `on` keyPressEvent   $ handleKeyBoard state
-    window  `on` exposeEvent    $ drawWindow window state
+    window `on` keyPressEvent   $ handleKeyBoard window state
+    window `on` exposeEvent     $ drawWindow window state
 
     onDestroy window mainQuit
     widgetShowAll window
