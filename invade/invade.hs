00988c3589b4e6840f63ac26b85f993c02b9980c
rockwolf <thereisanewway@gmail.com>
2013-03-07 08:26:51 +0100

    Started work on option parsing.

--- invade/invade.hs
+++ invade/invade.hs
@@ -11,6 +11,10 @@ Portability :  portable
 Makes the necessary calculations with regards to trading, investing and money management/risk management.
 -}
 
+import System.Console.GetOpt
+{-import Control.Monad
+import System.IO
+import Data.List-}
 import Data.Char
 import Data.Time.Clock
 import Data.Time.Calendar
@@ -201,8 +205,79 @@ calcCommission  market stockname price shares =
     -- TODO: getPredefined commission, based on type of input/commodity/market
     0.0
 
+{- CLI interfacing -}
+data Options = Options  { optVerbose    :: Bool
+                        , optInput      :: IO String
+                        , optOutput     :: String -> IO ()
+                        }
+
+startOptions :: Options
+startOptions = Options  { optVerbose    = False
+                        , optInput      = getContents
+                        , optOutput     = putStr
+                        }
+
+options :: [ OptDescr (Options -> IO Options) ]
+-- TODO: specify what kind of options I need.
+options =
+    [ Option "i" ["input"]
+        (ReqArg
+            (\arg opt -> return opt { optInput = readFile arg })
+            "FILE")
+        "Input file"
+ 
+    , Option "o" ["output"]
+        (ReqArg
+            (\arg opt -> return opt { optOutput = writeFile arg })
+            "FILE")
+        "Output file"
+ 
+    , Option "s" ["string"]
+        (ReqArg
+            (\arg opt -> return opt { optInput = return arg })
+            "FILE")
+        "Input string"
+ 
+    , Option "v" ["verbose"]
+        (NoArg
+            (\opt -> return opt { optVerbose = True }))
+        "Enable verbose messages"
+ 
+    , Option "V" ["version"]
+        (NoArg
+            (\_ -> do
+                hPutStrLn stderr "Version 0.01"
+                exitWith ExitSuccess))
+        "Print version"
+ 
+    , Option "h" ["help"]
+        (NoArg
+            (\_ -> do
+                                prg <- getProgName
+                hPutStrLn stderr (usageInfo prg options)
+                exitWith ExitSuccess))
+        "Show help"
+    ]
+
 main = do
-    -- TODO: option parsing
+    -- TODO: finish and test option parsing
+    args <- getArgs
+ 
+    -- Parse options, getting a list of option actions
+    let (actions, nonOptions, errors) = getOpt RequireOrder options args
+ 
+    -- Here we thread startOptions through all supplied option actions
+    opts <- foldl (>>=) (return startOptions) actions
+ 
+    let Options { optVerbose = verbose
+                , optInput = input
+                , optOutput = output   } = opts
+ 
+    when verbose (hPutStrLn stderr "Hello!")
+ 
+    -- TODO: instead of the below, set the Input to whats in the options.
+    input >>= output
+    
     let varInput = Input {
             i_pool = 104000.0 
             ,i_money_to_use = 5000.0
