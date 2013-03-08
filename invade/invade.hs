64da8658a1913c6f49f9e986419a58e4254af879
rockwolf <thereisanewway@gmail.com>
2013-03-08 15:59:31 +0100

    That FILE thingy was necessary?
    Also corrected some opt_ -> i_ mistakes.

--- invade/invade.hs
+++ invade/invade.hs
@@ -233,63 +233,63 @@ startOptions = Input  { i_verbose       = False
 options :: [ OptDescr (Input -> Input) ]
 options =
     [ Option "o" ["i_opt_pool"]
-        (ReqArg (\arg opt -> opt { i_pool = read arg }))
+        (ReqArg (\arg opt -> opt { i_pool = read arg }) "SOMETHING")
         "i_pool: pool at start"
 
     , Option "u" ["i_opt_money_to_use"]
-        (ReqArg (\arg opt -> opt { i_money_to_use = read arg }))
+        (ReqArg (\arg opt -> opt { i_money_to_use = read arg }) "SOMETHING")
         "i_money_to_use: money to use"
 
     , Option "p" ["i_opt_price"]
-        (ReqArg (\arg opt -> opt { i_price = read arg }))
+        (ReqArg (\arg opt -> opt { i_price = read arg }) "SOMETHING")
         "i_price: price"
   
     , Option "l" ["i_opt_long_short"]
-        (ReqArg (\arg opt -> opt { i_long_short = read arg }))
+        (ReqArg (\arg opt -> opt { i_long_short = read arg }) "SOMETHING")
         "i_long_short: 'L' or 'S' for long or short"
 
     , Option "s" ["i_opt_shares"]
-        (ReqArg (\arg opt -> opt { opt_shares = read arg }))
+        (ReqArg (\arg opt -> opt { i_shares = read arg }) "SOMETHING")
         "i_shares: shares"
 
     , Option "c" ["i_opt_commission"]
-        (ReqArg (\arg opt -> opt { opt_commission = read arg }))
+        (ReqArg (\arg opt -> opt { i_commission = read arg }) "SOMETHING")
         "i_commission: commission"
     
     , Option "t" ["i_opt_tax"]
-        (ReqArg (\arg opt -> opt { opt_tax = read arg }))
+        (ReqArg (\arg opt -> opt { i_tax = read arg }) "SOMETHING")
         "i_tax: tax"
 
     , Option "r" ["i_opt_risk"]
-        (ReqArg (\arg opt -> opt { opt_risk = read arg }))
+        (ReqArg (\arg opt -> opt { i_risk = read arg }) "SOMETHING")
         "i_risk: risk you are willing to take"
  
     , Option "m" ["i_opt_market"]
-        (ReqArg (\arg opt -> opt { opt_market = read arg }))
+        (ReqArg (\arg opt -> opt { i_market = read arg }) "SOMETHING")
         "i_market: market name"
  
     , Option "n" ["i_opt_stockname"]
-        (ReqArg (\arg opt -> opt { opt_stockname = read arg }))
+        (ReqArg (\arg opt -> opt { i_stockname = read arg }) "SOMETHING")
         "i_stockname: stock name"
  
     , Option "d" ["i_opt_spread"]
-        (ReqArg (\arg opt -> opt { opt_spread = read arg }))
+        (ReqArg (\arg opt -> opt { i_spread = read arg }) "SOMETHING")
         "i_spread: spread"
 
     , Option "x" ["i_opt_currency_from"]
-        (ReqArg (\arg opt -> opt { opt_currency_from = read arg }))
+        (ReqArg (\arg opt -> opt { i_currency_from = read arg }) "SOMETHING")
         "i_currency_from: currency from"
  
     , Option "y" ["i_opt_currency_to"]
-        (ReqArg (\arg opt -> opt { opt_currency_to = read arg }))
+        (ReqArg (\arg opt -> opt { i_currency_to = read arg }) "SOMETHING")
         "i_currency_to: currency to"
 
     , Option "e" ["i_opt_exchange_rate"]
-        (ReqArg (\arg opt -> opt { opt_exchange_rate = read arg }))
+        (ReqArg (\arg opt -> opt { i_exchange_rate = read arg }) "SOMETHING")
         "i_exchange_rate: exchange_rate"
  
     , Option "v" ["i_opt_verbose"]
-        (NoArg (\opt -> return opt { opt_verbose = True }))
+        (NoArg (\opt -> return opt { i_verbose = True }))
         "Enable verbose messages"
  
     , Option "V" ["i_opt_version"]
