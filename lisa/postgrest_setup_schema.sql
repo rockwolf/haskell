024203e4c68a9f48bbd36807bfa5046e185c8079
rockwolf <thereisanewway@gmail.com>
2015-01-23 13:59:58 +0100

    Update postgrest_setup_schema.sql

--- lisa/postgrest_setup_schema.sql
+++ lisa/postgrest_setup_schema.sql
@@ -3,3 +3,5 @@ CREATE SCHEMA “1”;
 ALTER TABLE t_trade SET SCHEMA “1”;
 ALTER TABLE t_rate SET SCHEMA “1”;
 ...
+-- Note: the alter statements can be left out, if the initial setup contains:
+-- create table "1".t_trade(...) etc.
