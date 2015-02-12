56a9258f9dd4ba6f9f5813fd912af1ae83398d2a
rockwolf <thereisanewway@gmail.com>
2015-02-12 22:03:50 +0100

    Mucho problemos to get this to compile again, but I figured out an issue: the mkdate stuff needs the second point too and they need to be doubles and not ints.

--- weightplot/weightplot.hs
+++ weightplot/weightplot.hs
@@ -26,7 +26,7 @@ import DateUtil(mkDate)
 -----------------------------------------------------------------------------
 -- ||| Declaration of datatypes
 -----------------------------------------------------------------------------
-C_IDEAL_WEIGHT = 74.0
+c_ideal_weight = 74.0
 
 -----------------------------------------------------------------------------
 -- ||| Plotting
@@ -38,23 +38,23 @@ C_IDEAL_WEIGHT = 74.0
 plotLines2 :: [[Double]] -> String -> [String] -> Bool -> Renderable ()
 plotLines2 plot_data title_main titles_series borders = toRenderable layout
   where
-    weight1 = plot_lines_style . line_color .~ (customColorSeq!!1)
-           $ plot_lines_values .~ [ [ (d,v) | (d,v,_) <- weightValues'] ]
-           $ plot_lines_title .~ (titles_series!!0)
-           $ def
-
-    weight2 = plot_lines_style . line_color .~ (customColorSeq!!2)
-           $ plot_lines_values .~ [ [ (d,v) | (d,_,v) <- weightValues'] ]
-           $ plot_lines_title .~ (titles_series!!1)
-           $ def
-
+--    weight1 = plot_lines_style . line_color .~ (customColorSeq!!1)
+--           $ plot_lines_values .~ [ [ (d,v) | (d,v,_) <- weightValues'] ]
+--           $ plot_lines_title .~ (titles_series!!0)
+--           $ def
+--
+--    weight2 = plot_lines_style . line_color .~ (customColorSeq!!2)
+--           $ plot_lines_values .~ [ [ (d,v) | (d,_,v) <- weightValues'] ]
+--           $ plot_lines_title .~ (titles_series!!1)
+--           $ def
+--
     layout = layoutlr_title .~ title_main
            $ layoutlr_plot_background .~ Just (solidFillStyle $ customColorSeq!!0)
            $ layoutlr_left_axis . laxis_override .~ axisGridHide
            $ layoutlr_right_axis . laxis_override .~ axisGridHide
            $ layoutlr_x_axis . laxis_override .~ axisGridHide
-           $ layoutlr_plots .~ [Left (toPlot weight1),
-                                Right (toPlot weight2)]
+--           $ layoutlr_plots .~ [Left (toPlot weight1),
+--                                Right (toPlot weight2)]
            $ layoutlr_grid_last .~ False
            $ def
  
@@ -62,7 +62,7 @@ plotLines2 plot_data title_main titles_series borders = toRenderable layout
                      , toAlphaColour (sRGB 0 255 0) -- weight 1
                      , toAlphaColour (sRGB 0 0 255) -- weight 2
           ]
-
+--
 -----------------------------------------------------------------------------
 -- ||| Data loading for plot
 -----------------------------------------------------------------------------
@@ -74,9 +74,9 @@ loadDataFromFile :: FilePath -> IO [Double]
 loadDataFromFile a_file_name = do
     l_file_data <- loadFileToStringList a_file_name
     -- l_file_data should be ["date1;v1a;v1b;comment1", "date2;v2a;v2b;comment2"] now
-    let l_split_data = splitLinesToElements l_file_data
+    --let l_split_data = splitLinesToElements l_file_data
     -- l_split_data should be ["date1", "v1a", ... , "comment2"] now
-    let l_only_doubles_as_string = map removeLastFromGroupedList $ map removeFirstFromGroupedList $ l_split_data
+    --let l_only_doubles_as_string = map removeLastFromGroupedList $ map removeFirstFromGroupedList $ l_split_data
     -- l_only_doubles_as_string should be ["v1a", "v1b", "v2a", "v2b"]
     return ["0.0", "0.1"]
 
@@ -95,7 +95,8 @@ loadData = do
     --let minimal_plot_data = convertListToListOfLists $ map addIdealWeightToGroupedList $ map removeLastFromGroupedList $ map removeFirstFromGroupedList $ file_data
     --let plot_data = addMissingMonths minimal_plot_data
     --let plot_data = minimal_plot_data
-    renderableToFile def to_file $ chart plot_data title_main titles_series True
+    --renderableToFile def to_file $ plotLines2 plot_data title_main titles_series True
+    renderableToFile def to_file $ plotLines2 [[81.4,74.0],[78.6,74.0]] title_main titles_series True
   where
     from_file = "data.dat"
     to_file = "data.png"
@@ -106,9 +107,9 @@ loadData = do
 -- | weightValues'
 -- | Transform the dates and filter the data.
 -----------------------------------------------------------------------------
-weightValues' :: [(LocalTime,Double,Double)]
-weightValues' day_start month_start year_start day_end month_end year_end = filterValues weightValues (
-    mkDate day_start month_start year_start) (mkDate day_end month_end year_end)
+--weightValues' :: [(LocalTime,Double,Double)]
+--weightValues' day_start month_start year_start day_end month_end year_end = filterValues weightValues (
+--    mkDate day_start month_start year_start) (mkDate day_end month_end year_end)
 
 -----------------------------------------------------------------------------
 -- | weightValues
@@ -116,8 +117,8 @@ weightValues' day_start month_start year_start day_end month_end year_end = filt
 --
 -- Example: [(10, 12, 2014, 80.1), (11, 12, 2014, 79.6)]
 -----------------------------------------------------------------------------
-weightValues :: [(Int, Int, Int, Int)] -> [(LocalTime,Double,Double)]
-weightValues plot_data_raw = [ (mkDate dd mm yyyy, p1) | (dd,mm,yyyy,p1) <- plot_data_raw ]
+weightValues :: [(Int, Int, Int, Double, Double)] -> [(LocalTime,Double,Double)]
+weightValues plot_data_raw = [ (mkDate dd mm yyyy, p1, p2) | (dd,mm,yyyy,p1, p2) <- plot_data_raw ]
 
 -----------------------------------------------------------------------------
 -- | filterValues
@@ -139,8 +140,8 @@ filterValues weight_values t1 t2 = [ v | v@(d,_,_) <- weight_values
 -- TODO: make ideal_weight a parameter and call it with the parameter, to make this function more generic.
 addIdealWeightToGroupedList [] = []
 addIdealWeightToGroupedList [x] = [x]
-addIdealWeightToGroupedList (x:y:[]) = [x] ++ [y] ++ [C_IDEAL_WEIGHT]
-addIdealWeightToGroupedList (x:y:xs) = [x] ++ [y] ++ [C_IDEAL_WEIGHT] ++ (addIdealWeightToGroupedList xs)
+addIdealWeightToGroupedList (x:y:[]) = [x] ++ [y] ++ [c_ideal_weight]
+addIdealWeightToGroupedList (x:y:xs) = [x] ++ [y] ++ [c_ideal_weight] ++ (addIdealWeightToGroupedList xs)
 
 -----------------------------------------------------------------------------
 -- | addMissingMonths
@@ -167,5 +168,6 @@ getMissingMonthsEmpty missing_months = take missing_months $ repeat [0,0,0]
 -----------------------------------------------------------------------------
 main :: IO (PickFn ())
 main = do
-    loadData
+    --loadData
+    putStrLn "dumbass"
     exitSuccess
