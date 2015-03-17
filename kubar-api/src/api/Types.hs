897ca5a23fa9589aabc3eb4ec963f8cccc879699
rockwolf <thereisanewway@gmail.com>
2015-03-17 15:45:42 +0100

    Update Types.hs

--- kubar-api/src/api/Types.hs
+++ kubar-api/src/api/Types.hs
@@ -6,16 +6,12 @@ module Api.Types where
 import           Control.Applicative
 import qualified Data.Text as T
 import           Data.Aeson
-import           Snap.Snaplet.PostgresqlSimple
 
+-- TODO: create datatypes for the calculationresult(s)
 data Todo = Todo
   { id   :: Int
   , text :: T.Text
   }
-
-instance FromRow Todo where
-  fromRow = Todo <$> field
-                 <*> field
 
 instance ToJSON Todo where
   toJSON (Todo id text) = object [ "id" .= id, "text" .= text ]
